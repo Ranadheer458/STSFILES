17c4402f26dc5f69eb2f331dbde2b68776fbb113
Ranadheerpowdapally <80275249+Ranadheerpowdapally@users.noreply.github.com>
2021-06-15 23:41:46 +0530

    firstone

---
+++ demo/src/main/java/org/springframework/boot/SpringApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot;
+
+import com.example.demo.DemoApplication;
+
+public class SpringApplication {
+
+	public static void run(Class<DemoApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
